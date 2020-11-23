248d4e9645d933ad48d199f6674306fe56f72883
Amanda Moreno <amandamoreno@MacBook-Air-de-Edison-2.local>
2020-11-23 20:44:56 -0300

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,18 @@ import React, {Component} from 'react';
 const Footer = () => {
   return (
     <footer class="col-md-12 footer navbar bg-dark">
-    ahuauahuahauhau
+      <div class="col-md-4">
+        <br/>SOF ltda direitos reservados<br/>
+      </div>
+      <div class="col-md-4">
+        <br/>Avenida dos Biomas Unidos, 1234<br/>
+        Conjunto Organizacional<br/>
+        São Pantanal - SP<br/>
+        Brasil<br/><br/>
+      </div>
+      <div class="col-md-4">
+        <br/>2020 - 2020<br/>
+      </div>
     </footer>
   );
 }
