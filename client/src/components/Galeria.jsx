ae6692dfb9d6bd6b828a84abeb4a97a68ed8edaf
blueheart <blueheart@tuta.io>
2020-11-28 18:20:50 -0300

    Galeria Lightbox adicionada

--- client/src/components/Galeria.jsx
+++ client/src/components/Galeria.jsx
@@ -1,32 +1,27 @@
 import React, {Component} from 'react';
 import Lateral from './Lateral';
-import GalleryImage from './GalleryImage';
+import { SRLWrapper} from 'simple-react-lightbox'
+import SimpleReactLightbox from 'simple-react-lightbox'
 
-const image = "./200x200.png";
-const desc = "Pantanal image"
 
 class Galeria extends Component {
 
   render() {
     const gallery_image = [];
     for (let i = 0; i < 12; i++) {
-      gallery_image.push(<GalleryImage short_desc={desc} img_src={image} ident={i} />);
+      gallery_image.push(<img class="mb-5 mx-4" width="300" height="200" alt ="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(117).jpg"/>);
     }
     return (
       <div class="container">
         <div class="row mt-5">
           <Lateral />
           <div class="col-md-9">
-          <div id="mdb-lightbox-ui">
-          </div>
             <div class="container">
-              <div class="row">
-                <div class="col-md-12">
-                  <div class="card-columns">
+                <SimpleReactLightbox>
+                  <SRLWrapper>
                     {gallery_image}
-                  </div>
-                </div>
-              </div>
+                  </SRLWrapper>
+                </SimpleReactLightbox>
             </div>
           </div>
         </div>
