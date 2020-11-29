fa907688033e32059cfca9472c68b77ebe680442
Amanda Moreno <amandamoreno@MacBook-Air-de-Edison-2.local>
2020-11-29 00:15:19 -0300

    adição do formulário e dados teste das ongs no json

--- client/src/components/Formulario.jsx
+++ client/src/components/Formulario.jsx
@@ -21,15 +21,19 @@ class Formulario extends React.Component {
     render() {
       return (
         <form onSubmit={this.handleSubmit}>
-          <label>
-            Nome:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Enviar" />
+            <div class="form-group">
+                <label for="exampleInputEmail1">Digite seu nome:</label>
+                <input type="text" class="form-control" value={this.state.value} onChange={this.handleChange} id="inputName1" aria-describedby="nameHelp" placeholder="Anônimo"/>
+                <small id="nameHelp" class="form-text text-muted">Deixe este espaço em branco se quiser publicar seu comentário anonimamente.</small>
+            </div>
+        <div class="form-group">
+            <label for="exampleInputTextarea1">Deixe seu comentário:</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Comentário" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
       );
     }
   }
 
   export default Formulario;
-  
--- client/src/components/Ong.jsx
+++ client/src/components/Ong.jsx
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import Lateral from './Lateral';
+import Formulario from './Formulario';
+import Comments from './Comments';
+import axios from 'axios';
 
 let text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit.\
 Nihil ad,ex eaque fuga minus reprehenderit asperiores earum incidunt.\
@@ -21,7 +24,28 @@ reprehenderit asperiores earum incidunt. Possimus maiores dolores voluptatum eni
 omnis debitis quam ab nemo necessitatibus."
 
 class Ong extends Component {
+
+  state = {ong:{}};
+  componentDidMount(){
+    this.getOng(this.props.ident);
+  }
+
+  getOng = (id) => {
+    axios.get(`/api/ongs/${id}`)
+      .then(res => {
+        if(res.data){
+          this.setState({
+            ong: res.data
+          });
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
+
   render() {
+    let {ong} = this.state;
+
     return (
       <div class="container mb-5">
         <div class="row mt-5">
@@ -35,9 +59,20 @@ class Ong extends Component {
                 </div>
                 <div class="text-left">
                   {text}
+                  {
+                    /*<h2>{ong.Nome}</h2>
+                    <p> {ong.Resumo}</p>
+                    <p><strong>Informações para contato:</strong></p>
+                    <p>Telefone: {ong.Telefone}</p>
+                    <p>Email: {ong.Email}</p>*/
+                  }      
                 </div>
               </div>
             </article>
+            <hr />
+              <Formulario />
+              <hr />
+              <Comments coms={ong.Comentarios}/>
           </div>
         </div>
       </div>
